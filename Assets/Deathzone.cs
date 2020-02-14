140e299d9f6c275ff10848ad5f3460681a438ba5
Jacky12898 <32112544+Jacky12898@users.noreply.github.com>
2020-02-13 23:02:06 -0700

    parallax effect added

---
+++ Assets/Deathzone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        if (player.transform.position.y <= this.transform.position.y)
+            Destroy(player);
+    }
+}
